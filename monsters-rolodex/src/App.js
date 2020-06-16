2acbb59dc8441e0f07f7c718c985261a6dd9835c
falcon2539 <alexfalconcompanies@gmail.com>
2020-06-15 19:33:53 -0700

    Created and stylized the card components, introduced the search bar.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-
 import { CardList } from './components/card-list/card-list.component';
-
 import './App.css';
 
 
@@ -11,7 +9,8 @@ class App extends Component {
     super();
     
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -22,13 +21,19 @@ class App extends Component {
   }
   
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className='App'>
-        <CardList name='Alex'>
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}> { monster.name } </h1>
-          ))}
-        </CardList>
+        <input 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
